5cb1c9a9937baec129cc6f01526e75d8b50a0055
Austin Seipp <aseipp@pobox.com>
2025-04-23 15:21:32 -0500

    egglog: use mimalloc
    Has a minor performance improvement in some benchmarks and is otherwise
    no worse on x86_64-linux (as usual.)
    
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- buck/third-party/egglog/main.rs
+++ buck/third-party/egglog/main.rs
@@ -1,6 +1,9 @@
 // SPDX-FileCopyrightText: © 2024-2025 Austin Seipp, 2024-2025 Egglog Authors
 // SPDX-License-Identifier: Apache-2.0
 
+#[global_allocator]
+static GLOBAL_ALLOCATOR: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
 fn main() {
     egglog::cli(egglog::EGraph::default())
 }
