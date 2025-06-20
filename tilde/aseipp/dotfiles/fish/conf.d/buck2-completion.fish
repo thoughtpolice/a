9372f573d6a062685d9a9799ae22fb9b52703261
Austin Seipp <aseipp@pobox.com>
2025-06-19 21:16:38 -0500

    tilde: add more fish config files
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

---
+++ tilde/aseipp/dotfiles/fish/conf.d/buck2-completion.fish
@@ -0,0 +1,15 @@
+set -g __buck2_completion_loaded false
+
+function __check_buck2_availability --on-variable PATH
+    # Avoid reloading if already loaded
+    if $__buck2_completion_loaded
+        return
+    end
+
+    # Check if buck2 is now available
+    if command -q buck2
+        # Generate and load completions
+        buck2 completion fish | source
+        set -g __buck2_completion_loaded true
+    end
+end
--- tilde/aseipp/dotfiles/fish/config.fish
+++ tilde/aseipp/dotfiles/fish/config.fish
@@ -3,4 +3,5 @@
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    direnv hook fish | source
 end
